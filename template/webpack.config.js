dd34126a4a5f2dbcb72e3b73a02db3840ed579c4
ihanyang <h513915503@163.com>
2016-05-30 09:54:33 +0800

    Update webpack.config.js

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,16 +1,13 @@
-var path = require("path")
-var vue = require("vue-loader")
-var webpack = require("webpack")
-var postcss = require("postcss-loader")
-var autoprefixer = require("autoprefixer")
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
+import precss from "precss"
+import autoprefixer from "autoprefixer"
+import ExtractTextPlugin from "extract-text-webpack-plugin"
 
-module.exports = {
+export default {
 	entry: "./src/app.js",
 	output: {
-		path: "D:/wamp/www/wanba/openplatform/assets/",
-		filename: "app.js",
-		publicPath: "/assets/"
+		path: "",
+		filename: "build.js",
+		publicPath: ""
 	},
 	module: {
 		loaders: [
@@ -28,25 +25,29 @@ module.exports = {
 				loader: ExtractTextPlugin.extract("style-loader", "css!postcss")
 			},
 			{
-				test: /\.(png|jpg|gif)$/,
-				loader: "url-loader?limit=8192"
+				test: /\.(png|jpg|gif|ttf|svg|ico)$/,
+				loader: "url-loader",
+				query: {
+					name: "[hash].[ext]",
+					limit: 10000,
+				}
 			}
 		]
 	},
-	babel: {
-		presets: ["es2015", "stage-3"],
-		plugins: ["transform-runtime"]
-	},
 	postcss: [
+		precss,
 		autoprefixer({
-			browsers: "IE 10"
+			browsers: "> 5%"
 		})
 	],
 	plugins: [
-		new ExtractTextPlugin("[contenthash].css")
+		new ExtractTextPlugin("mobile.css")
 	],
+	resolve: {
+		extensions: ["", ".js", ".vue", "css"]
+	},
 	externals: {
-		Vue: "Vue"
+		vue: "Vue"
 	},
 	devtool: "source-map"
 }
